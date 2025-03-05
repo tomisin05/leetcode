7569892f868cf4c9e16c88c7a6241cf255dc9413
Oluwatomisin John Badmus <155178644+tomisin05@users.noreply.github.com>
2025-03-04 23:31:54 -1200

    update

--- leetcode.java
+++ leetcode.java
@@ -7,7 +7,7 @@ public boolean isP
 
 // roman numeral
 
-'''first crea te a Ma p wi th th e ro ma n nu me ra ls an d th ei r re sp ec ti ve values
+'''first create a Ma p wi th th e ro ma n nu me ra ls an d th ei r re sp ec ti ve values
 then create a character array  of the input string
 create variable for result 
 initialize two pointer i=0 and j =1, then loop till j < length of the character array
